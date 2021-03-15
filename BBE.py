686c6ce2c58f69dbee77d0108cdf9f3da813ef7c
James <james@hawkinsplace.co.uk>
2021-03-15 18:47:35 +0000

    initial commit

---
+++ BBE.py
@@ -0,0 +1,50 @@
+from DTOs import *
+
+
+class BettingExchangeView(object):
+    def add_bet(self, bet: Bet) -> None:
+        pass
+
+class Bettor(object):
+    def __init__(self, id: int):
+        self.id = id
+
+    def respond(exchange: BettingExchangeView):
+        pass
+
+class BettingExchange(BettingExchangeView):
+    def __init__(self, event_ids: list):
+        # Construct empy LOB
+        self.bets = {}
+        self.lob = {
+            event_id : EventBets(event_id) for event_id in event_ids
+        }
+        self.lob_anon = {
+            event_id : EventBets(event_id) for event_id in event_ids
+        }
+        self.observers = []
+
+    def add_observer(self, bettor: Bettor) -> None:
+        self.observers.append(bettor)
+
+    def __notify_obeservers(self) -> None:
+        map(lambda bettor: bettor.respond(self), self.observers)
+
+    def add_bet(self, bet: Bet) -> None:
+        self.lob[bet.event_id].add_bet(bet)
+        self.bets[bet.id] = bet
+        self.__notify_obeservers()
+
+    def distribute_winnings(event_winner_id: int) -> None:
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    exchange = BettingExchange([i+1 for i in range(20)])
+    bet = Back(12, 102, 12, 12)
+    b = Backs()
+    b.test()
+    print(bet)
---
+++ DTOs.py
@@ -0,0 +1,59 @@
+class Bet(object):
+    def __init__(self, bettor_id: int, event_id: int, odds: int, stake: int):
+        self.bettor_id = bettor_id
+        self.event_id = event_id
+        self.odds = odds
+        self.stake = stake
+        self.time = 0 # TODO
+    
+    def __str__(self):
+        return '{bet_type=%s, bettor_id=%d, event_id=%d, odds=%5.f, stake=%d, time=%f}' % \
+               (self._bet_type, self.bettor_id, self.event_id, self.odds, self.stake, self.time)
+
+class Back(Bet):
+    def __init__(self, bettor_id: int, event_id: int, odds: int, stake: int):
+        super().__init__(bettor_id, event_id, odds, stake)
+        self._bet_type = "Back"
+
+class Lay(Bet):
+    def __init__(self, bettor_id: int, event_id: int, odds: int, stake: int):
+        super().__init__(bettor_id, event_id, odds, stake)
+        self._bet_type = "Lay"
+
+class OddsOverview(object):
+    def __init__(self, odds: int):
+        self.odds = odds
+        self.bets = []
+
+    def add_bet(self, bet: Bet):
+        self.bets.append(bet)
+
+class OrderBookHalf(object):
+    def __init__(self):
+        self.total_stakes = {}
+        self.bets = {}
+
+    def add_bet(self, bet: Bet):
+        if bet.odds in self.total_stakes:
+            self.total_stakes[bet.odds] += bet.stake
+            self.bets[bet.odds].append(bet)
+        else:
+            self.total_stakes[bet.odds] = bet.stake
+            self.bets[bet.odds] = [bet]
+        
+
+class EventBets(object):
+    def __init__(self, event_id: int):
+        self.event_id = event_id
+        self.backs = OrderBookHalf()
+        self.lays = OrderBookHalf()
+
+    def __str__(self):
+        return '{event_id=%d, backs=%s, lays=%s}' % \
+               (self.event_id, self.best_back, self.best_lay, self.backs, self.lays)
+
+    def add_bet(self, bet: Back) -> None:
+        self.backs.add_bet(bet)
+
+    def add_bet(self, bet: Lay) -> None:
+        self.lays.add_bet(bet)
