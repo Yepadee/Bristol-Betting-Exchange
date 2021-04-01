fixed winning calculation for lays

BBE.py
@@ -57,10 +57,9 @@ if __name__ == "__main__":
 
     '''Add Bettors'''
     bettors = {}
-    for i in range(1,9):
-        n_sims = 2 ** np.random.randint(1, 9)
-        bettors[i] = NaiveBettor(id=i, balance=100, num_simulations=n_sims)
-
+    for i in range(100):
+        n_sims = 2 ** np.random.randint(2, 9)
+        bettors[i] = NaiveBettor(id=i, balance=10000, num_simulations=n_sims)
     # bettors[1] = NaiveBettor(id=1, balance=500, num_simulations=8)
     # bettors[2] = NaiveBettor(id=2, balance=500, num_simulations=16)
     # bettors[3] = NaiveBettor(id=3, balance=500, num_simulations=32)
@@ -101,37 +100,38 @@ if __name__ == "__main__":
             lob_view = exchange.get_lob_view()
 
             if new_bet is not None: # If they want to post a new bet
-                print("-----------------------------")
-                print("before: %s" % str(new_bet))
+                # print("-----------------------------")
+                # print("before: %s" % str(new_bet))
                 matched_this_bet = []
                 bet_cost = exchange.add_bet(new_bet, matched_this_bet) # Add it to the exchange, and retrieve the bets it was matched with (if any) and the cost of placing the bet.
-                print("after: %s" % str(new_bet))
-                print("cost: %d" % bet_cost)
-                print("matched: ")
-                for mb in matched_this_bet:
-                    print(mb)
-                print("-----------------------------\n")
+                # print("after: %s" % str(new_bet))
+                # print("cost: %d" % bet_cost)
+                # print("matched: ")
+                # for mb in matched_this_bet:
+                #     print(mb)
+                # print("-----------------------------\n")
                 rdm_bettor.deduct_funds(bet_cost) # Charge the bettor the cost of the bet. (May be less then estimated cost if it was a lay bet and matched with bets with better odds)
                 matched_bets.extend(matched_this_bet)
                 bettor: Bettor
                 for bettor in bettor_list: # 
                     bettor.on_bets_matched(lob_view, percent_complete, matched_bets)
 
-        t += 1
+        t += opinion_update_period
 
-    #exchange.
-    print(exchange)
+    #print(exchange)
     for bettor in bettor_list:
         bettor.cancel_unmatched()
 
     distribute_winnings(bettors, matched_bets, race.get_winner())
-    #print(exchange)
     bettor_list.sort(reverse=True, key=(lambda b: b.get_balance()))
     sum_bal = 0
     b: Bettor
     for b in bettor_list:
         sum_bal += b.get_balance()
         print(b)
+
+    for mb in matched_bets:
+        print(mb)   
 
     print(sum_bal)
     print(race.get_winner())

bettors.py
@@ -71,12 +71,15 @@ class Bettor(object):
 
     def cancel_unmatched(self) -> None:
         back: Back
+        sum_refund = 0
         for back in self.__backs:
             self.__balance += back.get_unmatched()
+            sum_refund += back.get_unmatched()
 
         lay: Lay
         for lay in self.__lays:
             self.__balance += lay.get_unmatched_liability()
+            sum_refund += lay.get_unmatched_liability()
 
     # The following methods must be implemented in a new betting agent:
     def get_bet(self, lob_view: dict) -> Bet:
@@ -114,8 +117,8 @@ class NaiveBettor(Bettor):
         if self.__last_event_probs is not None and self.get_balance() > 0:
             rdm = np.random.randint(0, 2)
             if rdm == 0:
-                stake = np.random.randint(1, self.get_balance() + 1)
-                if stake > 0:
+                if self.get_balance() > 200:
+                    stake = np.random.randint(200, self.get_balance() + 1)
                     best_competetor = np.argmax(self.__last_event_probs)
                     odds = round((1.0 / self.__last_event_probs[best_competetor]) * 100)
                     if odds == 100:
@@ -129,9 +132,9 @@ class NaiveBettor(Bettor):
                 prob = self.__last_event_probs[worst_competetor]
                 if prob < 0.99:
                     odds = round((1.0 / prob) * 100)
-                    max_stake = self.get_balance() // (odds // 100) #TODO: fix max_stake exceeding balance
-                    if max_stake > 1:
-                        stake = np.random.randint(1, max_stake)
+                    max_stake = int(self.get_balance() / (odds / 100.0)) #TODO: fix max_stake exceeding balance
+                    if max_stake > 200:
+                        stake = np.random.randint(200, max_stake)
                         return self._new_lay(worst_competetor + 1, odds, stake)
         
         return None

lob.py
@@ -26,7 +26,7 @@ class MatchedBet(object):
         return self.__layer_id
 
     def get_layer_winnings(self) -> int:
-        return self.__stake + self.get_backer_winnings() # Return layed stake plus liability
+        return self.get_backer_winnings() # Return layed stake plus liability
 
     def get_backer_winnings(self) -> int:
         return self.__odds * self.__stake // 100
@@ -144,7 +144,6 @@ class OrderBookHalf(object):
         qty_matched = new_bet_stake - qty_unmatched
         new_bet.reduce_stake(qty_matched) # Update stake since some of the stake has now been filled by better odds
         bet_cost += self._get_bet_cost(odds, new_bet.get_unmatched())
-
         return bet_cost
 
     def _get_bet_cost(self, odds: int, stake: int) -> int:
@@ -172,7 +171,7 @@ class OrderBookHalf(object):
 
 class BackOrderBook(OrderBookHalf):
     def _get_bet_cost(self, odds: int, stake: int) -> int:
-        return odds * stake // 100
+        return odds * stake // 100 - stake
 
     def _get_ordered_odds(self) -> np.array(np.int32): 
         odds = np.array(list(self._total_stakes.keys()))
@@ -221,15 +220,15 @@ class OrderBook(object):
         other_book: OrderBookHalf = None
         
         if type(bet) is Back:
-            other_book = self.__lays
             order_book = self.__backs
+            other_book = self.__lays
         elif type(bet) is Lay:
-            other_book = self.__backs
             order_book = self.__lays
+            other_book = self.__backs
         else:
             raise Exception("Invalid bet type %s!" % str(type(bet)))
 
-        bet_cost = other_book.match_bet_better_odds(bet, matched_bets) # Attempt to match the bet with bets on the other side of the lob
+        bet_cost = other_book.match_bet_better_odds(bet, matched_bets) # Attempt to match the bet with bets on the other side of the lob with better odds than those requested
         total_unmatched = other_book.match_bet_at_odds(bet, matched_bets)
         if total_unmatched > 0: # If the bet wasnt totally matched, add it to the lob in the hope it can be matched by future bets
             order_book.add_bet(bet)